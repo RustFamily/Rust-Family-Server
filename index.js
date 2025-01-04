3d5c350d5d182454372601f2bf6381f5b3c05990
Rust Family <jrshaiks7@Gmail.com>
2025-01-04 11:39:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎮 Jogando Rust Famly | 2x - Squad", "🎮 Jogando Rust Famly | 2x - Simplesmente o melhor!"];
+const statusMessages = ["🎮 Jogando Rust Famly | 2x - Squad", "🎮 Jogando Rust Family - Simplesmente o melhor!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
